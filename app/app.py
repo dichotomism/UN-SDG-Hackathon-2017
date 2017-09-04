e78c20c34cca713d435f59fb1be39d575c91bf02
mathematiguy <cmos032@aucklanduni.ac.nz>
2017-09-05 04:50:05 +1200

    improved map in app.py and implemented click selection

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import json
 
 import plotly.graph_objs as go
 
@@ -10,12 +12,23 @@ app = dash.Dash()
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#FFFFFF',
+    'text': '#111111'
+}
+
+styles = {
+    'column': {
+        'display': 'inline-block',
+        'width': '33%',
+        'padding': 10,
+        'boxSizing': 'border-box',
+        'minHeight': '200px'
+    },
+    'pre': {'border': 'thin lightgrey solid'}
 }
 
 # create base map for country selection
-exec(open("map.py").read())
+exec(open("map/map.py").read())
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
@@ -34,8 +47,36 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     dcc.Graph(
         id = 'world-map',
         figure=fig
-    )
+    ),
+
+    # html.Div([
+    #     dcc.Markdown(
+    #         html.Pre(id = 'click-data', style = styles['pre']))
+    #     ], style = styles['column']),
+
+    html.Div([
+        dcc.Markdown("""
+            **Click Data**
+
+            Click on points in the graph.
+        """.replace('    ', '')),
+        html.Pre(id='click-data'),
+    ]),
+
 ])
+
+@app.callback(
+    Output('click-data', 'children'),
+    [Input('world-map', 'clickData')])
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+
+# @app.callback(
+#     Output('click-data', 'children'),
+#     [Input('world-map', 'clickData')])
+# def display_click_data(clickData):
+#     return json.dumps(clickData, indent=2)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- app/map/map.py
+++ app/map/map.py
@@ -1,12 +1,15 @@
 import plotly.plotly as py
+import plotly.graph_objs as go
+
+import numpy as np
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
+df = pd.read_csv('map/country_codes.csv')
 
 data = [ dict(
         type = 'choropleth',
         locations = df['CODE'],
-        z = df['GDP (BILLIONS)'],
+        z = np.ones(len(df)),
         text = df['COUNTRY'],
         colorscale = [[0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
             [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"]],
@@ -17,21 +20,16 @@ data = [ dict(
                 color = 'rgb(180,180,180)',
                 width = 0.5
             ) ),
-        colorbar = dict(
-            autotick = False,
-            tickprefix = '$',
-            title = 'GDP<br>Billions US$'),
+        showscale = False,
       ) ]
 
 layout = dict(
-    title = '2014 Global GDP<br>Source:\
-            <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html">\
-            CIA World Factbook</a>',
+    title = 'Select a country:',
     geo = dict(
         showframe = False,
-        showcoastlines = False,
+        showcoastlines = True,
         projection = dict(
-            type = 'Robinson'
+            type = 'robinson'
         )
     )
 )
